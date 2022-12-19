6eeb801789912ed707806ed0ff37549ce8d0b69d
hyeonsuu <hyeonsu.dev@gmail.com>
2022-12-19 18:13:04 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,15 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
 		int a, b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a + b);
-	}
+		
+		Scanner ss = new Scanner(System.in);
+		
+		a = ss.nextInt();
+		b = ss.nextInt();
+
+        System.out.println(a + b);
+    }
 }
