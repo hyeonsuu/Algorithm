0771b8f07c71d32346af6000898231fb1e10ac48
hyeonsuu <hyeonsu.dev@gmail.com>
2024-01-30 08:57:11 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+        
+        
+        
+        
+        return answer;
+    }
+}
